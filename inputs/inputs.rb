189bdf5e2d0c7999e145d614a61dfe3b2f63b0a1
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2023-02-06 15:21:14 +0600

    Update inputs.rb

--- inputs/inputs.rb
+++ inputs/inputs.rb
@@ -1,7 +1,3 @@
-@mubinwl12c = 'https://mubinwl12c.therapbd.net:7002'
-@develop = 'https://develop.therapbd.net'
-@staging = 'https://staging.therapbd.net'
-@master = 'https://master.therapbd.net'
 @bakhtiar = 'https://192.168.0.101'
 @bakhtiar2 = 'https://192.168.0.165'
 @erfan = 'https://192.168.0.47'
