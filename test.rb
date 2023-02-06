ddbb5ec77d8d32e98b6f3b334865e725a1bbd38d
Nawshad Rehan Rasha <rasha-cse@users.noreply.github.com>
2023-02-06 15:20:23 +0600

    Delete test.rb

--- test.rb
+++
@@ -1,7 +0,0 @@
-require 'roo'
-require 'spreadsheet'
-
-s1 = Roo::Spreadsheet.open('inputs/Letter_Search.xlsx')
-(s1.first_row..s1.last_row).each do |row|
-  puts s1.cell(row,1).to_s.chomp + " " + s1.cell(row,2).to_s.chomp
-end
